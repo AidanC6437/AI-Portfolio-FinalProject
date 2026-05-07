11c57e8d4612bd3608df9af8198e4b4a63d5b895
Aidan Chiu <aidan_chiu1@baylor.edu>
2026-05-07 17:40:25 -0500

    Static Fix

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ set -o errexit
 # Run migrations
 python manage.py migrate
 
+# Prepare static assets for Render/WhiteNoise
+python manage.py collectstatic --no-input
+
 # Start gunicorn server
 gunicorn portfolio_project.wsgi:application --bind 0.0.0.0:${PORT:-8000}
-
 
